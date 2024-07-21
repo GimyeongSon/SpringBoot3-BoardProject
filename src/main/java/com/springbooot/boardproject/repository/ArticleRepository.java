4b3e5951b59082e61fcd4f4f89414747d38d0381
SonGimyeong <dev.songm@gmail.com>
2024-07-22 01:52:02 +0900

    게시판 Read - 특정 게시글 조회 - 모든 게시글 조회

--- src/main/java/com/springbooot/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/springbooot/boardproject/repository/ArticleRepository.java
@@ -3,5 +3,9 @@ package com.springbooot.boardproject.repository;
 import com.springbooot.boardproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll(); // Iterable -> ArrayList
 }
