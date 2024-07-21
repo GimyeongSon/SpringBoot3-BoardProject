f118e234c9aeacaf5b31ea6cfea27853922df879
SonGimyeong <dev.songm@gmail.com>
2024-07-21 21:25:27 +0900

    프로젝트 셋팅

---
+++ src/main/java/com/springbooot/boardproject/SpringBootBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springbooot.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBoardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbooot/boardproject/SpringBootBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbooot.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
