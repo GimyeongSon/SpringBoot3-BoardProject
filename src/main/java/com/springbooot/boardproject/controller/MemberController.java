40926d9941cabb7cf5b624984494d682b69899e4
SonGimyeong <dev.songm@gmail.com>
2024-07-22 01:59:06 +0900

    유저 Read - 특정 유저 조회 - 모든 유저 조회

--- src/main/java/com/springbooot/boardproject/controller/MemberController.java
+++ src/main/java/com/springbooot/boardproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.springbooot.boardproject.repository.MemberRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @Log4j2
@@ -27,5 +31,18 @@ public class MemberController {
         Member saved = memberRepository.save(member);
         log.info(saved);
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+    }
+    @GetMapping("/members")
+    public String index(Model model){
+        List<Member> memberList = memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+        return "members/index";
     }
 }
--- src/main/java/com/springbooot/boardproject/repository/MemberRepository.java
+++ src/main/java/com/springbooot/boardproject/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.springbooot.boardproject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.springbooot.boardproject.entity.Member;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll();
 }
