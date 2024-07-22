e19d4c286e3f0cf14766261f6b5d9b928bfed9c4
SonGimyeong <dev.songm@gmail.com>
2024-07-22 23:58:46 +0900

    Update - 게시글 수정관련 코드작성

--- src/main/java/com/springbooot/boardproject/controller/ArticleController.java
+++ src/main/java/com/springbooot/boardproject/controller/ArticleController.java
@@ -39,6 +39,23 @@ public class ArticleController {
         return "redirect:/articles/" + saved.getId();
     }
 
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        Article articleEntity = form.toEntity();
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if(target != null) {
+            articleRepository.save(articleEntity);
+        }
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model){
         log.info("id = " + id);
