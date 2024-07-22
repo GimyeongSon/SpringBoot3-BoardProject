7d6330215988f2e841aa52fadbe873e23f0d63e7
SonGimyeong <dev.songm@gmail.com>
2024-07-23 03:18:57 +0900

    회원, 게시판 Delete - 회원, 게시판 삭제관련 코드작성

--- src/main/java/com/springbooot/boardproject/controller/MemberController.java
+++ src/main/java/com/springbooot/boardproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -47,6 +48,16 @@ public class MemberController {
             memberRepository.save(memberEntity);
         }
         return "redirect:/members/" + memberEntity.getId();
+    }
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        if(memberEntity!=null){
+            memberRepository.delete(memberEntity);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+
+        return "redirect:/members";
     }
     @GetMapping("/members/{id}")
     public String show(@PathVariable Long id, Model model){
